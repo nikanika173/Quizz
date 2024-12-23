d0a188b220adcb820d66b0ccb678b1aa05a9751f
nikanika173 <147316302+nikanika173@users.noreply.github.com>
2024-12-23 20:35:47 +0400

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+        question: "What is the highest mountain in the world?",
+        option1: "K2",
+        option2: "Mount Everest",
+        option3: "Mount Kilimanjaro",
+        option4: "Denali",
+        ans: 2
+    },
+    {
+        question: "Which piece of equipment is essential for climbing steep ice-covered slopes?",
+        option1: "Carabiner",
+        option2: "Ice Axe",
+        option3: "Rope",
+        option4: "Harness",
+        ans: 2
+    },
+    {
+        question: "What is the term for the process of acclimating to high altitudes to avoid altitude sickness?",
+        option1: "Conditioning",
+        option2: "Altitude Training",
+        option3: "Acclimatization",
+        option4: "Resilience Building",
+        ans: 3
+    },
+    {
+        question: "Which famous mountain range is home to the 'Seven Summits'?",
+        option1: "The Andes",
+        option2: "The Alps",
+        option3: "The Himalayas",
+        option4: "The Rocky Mountains",
+        ans: 3
+    },
+    {
+        question: "What is a 'base camp' in mountain climbing?",
+        option1: "A temporary shelter halfway up a mountain",
+        option2: "The final point before summiting",
+        option3: "The main starting point for an ascent",
+        option4: "A rest area during descents",
+        ans: 3
+    },
+    {
+        question: "Who was the first person to summit Mount Everest?",
+        option1: "George Mallory",
+        option2: "Edmund Hillary",
+        option3: "Reinhold Messner",
+        option4: "Tenzing Norgay",
+        ans: 2
+    },
+    {
+        question: "What natural hazard is a major risk during mountain climbing?",
+        option1: "Floods",
+        option2: "Avalanches",
+        option3: "Tornadoes",
+        option4: "Earthquakes",
+        ans: 2
+    },
+    {
+        question: "What is the name of the climbing technique where climbers ascend vertical rock faces using their hands and feet?",
+        option1: "Bouldering",
+        option2: "Free Climbing",
+        option3: "Belaying",
+        option4: "Rappelling",
+        ans: 2
+    },
+    {
+        question: "Which mountain is known as the 'Savage Mountain' due to its difficulty and high fatality rate?",
+        option1: "Mount Everest",
+        option2: "K2",
+        option3: "Mount McKinley",
+        option4: "Annapurna",
+        ans: 2
+    },
+    {
+        question: "What is the primary reason climbers carry oxygen tanks on high-altitude climbs?",
+        option1: "To improve stamina",
+        option2: "To combat altitude sickness",
+        option3: "To prevent dehydration",
+        option4: "To keep warm",
+        ans: 2
+    }
+];
